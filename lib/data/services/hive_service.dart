700d03e97a10c1a2554cdd804d416ca380b16ea3
Mateus Leal <mateus.c.leal@outlook.com>
2025-06-30 17:14:40 -0300

    Subindo o projeto azs-mob-rickandmorty

--- lib/data/services/hive_service.dart
+++ lib/data/services/hive_service.dart
@@ -17,6 +17,7 @@ class HiveManager {
     for (int i = 0; i < keys.length; i++) {
       listKv[keys.elementAt(i)] = values.elementAt(i);
     }
+
   }
 
   static Future<void> setValue(int key, List < bool> value) async {
